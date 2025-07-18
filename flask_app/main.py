902385c5382ecaf9d670e7eb78e8c4ba7360789e
Chidera Okoye <106495757+deecodz@users.noreply.github.com>
2025-07-18 10:00:32 +0100

    Update main.py

--- flask_app/main.py
+++ flask_app/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "A couple 100 internet documentations and I are Devops Engineers! lol"
+    return "A couple 100 internet documentations and I are Devops Engineers! lol \n This is hosted on google cloud run... yaaaay!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int("5000"), debug=True)
